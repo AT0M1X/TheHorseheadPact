c2ac645405ae34b94a8a24550e91a70a1d07cb32
Mathias <mfaldt17@student.aau.dk>
2025-11-15 14:46:26 +0100

    turn manager

--- autoload/TurnManager.gd
+++ autoload/TurnManager.gd
@@ -5,6 +5,11 @@ var enemies := []
 var bullets := []
 var state := "player" # "player" or "enemies"
 var game_over := false
+var currentTurn = 0
+var dealTurnModifier = 5
+
+var unusedDealList := []
+var usedDealList := []
 
 func reset():
 	enemies.clear()
@@ -20,10 +25,13 @@ func register_enemy(e):
 func on_player_end_turn():
 	if game_over:
 		return
+	
+	if(state == "player"):
+		state = "enemies"
 		
-	state = "enemies"
-	process_bullets()
-	process_enemies()
+		next_turn()
+		process_bullets()
+		process_enemies()
 
 func process_enemies():
 	for e in enemies:
@@ -49,3 +57,20 @@ func player_die():
 	
 	game_over = true
 	player.die()
+
+func process_deal_turn():
+	state = "deal"
+	print("horse")
+	emit_signal("dealTurnHappened")
+	state = "enemy"
+
+func next_turn():
+	currentTurn += 1
+	if (currentTurn % (dealTurnModifier + randi_range(-2,2)) == 0):
+		process_deal_turn()
+		
+func pick_deal ():
+	return
+
+func reset_deals ():
+	return
