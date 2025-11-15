b450dbc67e55efbfecfed77e1e27bc7e8b3618a9
iRandz <chrbj-@hotmail.com>
2025-11-15 19:31:34 +0100

    fixed bullet phasing

--- scripts/actors/Bullet.gd
+++ scripts/actors/Bullet.gd
@@ -8,18 +8,32 @@ func setup(dir: Vector2, pos: Vector2):
 	direction = dir
 	position = pos
 	TurnManager.register_bullet(self)
-	check_collision()
+	check_collision(false)
 
 func move():
 	if spawn_turn:
 		spawn_turn = false
 		return
 		
+<<<<<<< Updated upstream
 	position += direction * tile_size
 	check_collision()
+=======
+	check_collision(true)
+	if raycast.is_colliding():
+		var hit = raycast.get_collider()
+		if hit != TurnManager.player and !hit.is_in_group("enemies") and !hit.is_in_group("projectiles"):
+			queue_free()
+			TurnManager.bullets.erase(self)
+		
+	position += direction * Settings.tile_size
+	check_collision(false)
+>>>>>>> Stashed changes
 
-func check_collision():
+func check_collision(pre_check: bool):
+	if !get_tree(): return
 	var bodies = get_tree().get_nodes_in_group("enemies")
+<<<<<<< Updated upstream
 	for e in bodies:
 		if e.position == position:
 			queue_free()
@@ -27,10 +41,29 @@ func check_collision():
 			e.queue_free()
 			TurnManager.enemies.erase(e)
 			break
+=======
+>>>>>>> Stashed changes
 	
-	if TurnManager.player:
-		var p = TurnManager.player
-		if p.position == position:
-			queue_free()
-			TurnManager.bullets.erase(self)
+	if !pre_check:
+		for e in bodies:
+			if e.position == position:
+				remove_bullet()
+				TurnManager.kill_enemy(e)
+				return
+			
+	var p = TurnManager.player
+
+	if p.position == position:
+		if pre_check:
+			if p.last_dir == -direction:
+				remove_bullet()
+				TurnManager.player_die()
+				return
+		else:
+			remove_bullet()
 			TurnManager.player_die()
+			return
+
+func remove_bullet():
+	queue_free()
+	TurnManager.bullets.erase(self)
