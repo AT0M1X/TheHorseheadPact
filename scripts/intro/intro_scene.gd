fc996c3d0dcf6c7d7eb7da6e0a4f09f525a2e42e
mr poopoopeepee <34302236+lockeyhannah@users.noreply.github.com>
2025-11-16 11:20:04 +0100

    Added intro scene

---
+++ scripts/intro/intro_scene.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _ready():
+	$VideoStreamPlayer.play()
+	$VideoStreamPlayer.finished.connect(_on_video_finished)
+
+func _on_video_finished():
+	get_tree().change_scene_to_file("res://scenes/main/Main.tscn")
